b4ffd11dbb0a1390a7ae80c83b0d280563dc0e57
Roman Denisov <99988283+Fenridan@users.noreply.github.com>
2023-05-02 01:35:01 +0300

    Update textgen.cpp

--- src/textgen.cpp
+++ src/textgen.cpp
@@ -9,18 +9,16 @@ Gen::Gen(std::string array, int val1, int val2) {
     data = array;
     int i = 0, state = 0, count = -1;
     while (array[i] != '\0') {
-        if (state == 0 && array[i] != ' ' && 
+        if (state == 0 && array[i] != ' ' &&
             array[i] != '\r' && array[i] != '\n') {
             state = 1;
             count++;
             words.push_back("");
             words[count] = words[count] + array[i];
-        }
-        else if (state == 1 && (array[i] == ' ' || 
+        } else if (state == 1 && (array[i] == ' ' ||
             array[i] == '\r' || array[i] == '\n')) {
             state = 0;
-        }
-        else if (state == 1 && array[i] != ' ' && 
+        } else if (state == 1 && array[i] != ' ' &&
             array[i] != '\r' && array[i] != '\n') {
             words[count] = words[count] + array[i];
         }
@@ -45,14 +43,14 @@ std::string Gen::getText() {
                 answer = answer + str[i] + ' ';
         }
         int val;
-        if (statetab[str].size() == 1 && statetab[str][0] == "END_OF_FILE") 
+        if (statetab[str].size() == 1 && statetab[str][0] == "END_OF_FILE")
             break;
         if (statetab[str].size() == 0) break;
         val = rand() % statetab[str].size();
-        if (statetab[str][statetab[str].size() - 1] == "END_OF_FILE") 
+        if (statetab[str][statetab[str].size() - 1] == "END_OF_FILE")
             val = rand() % (statetab[str].size() - 1);
         answer = answer + statetab[str][val] + ' ';
-        if (count * 100 - answer.size() < 0 || 
+        if (count * 100 - answer.size() < 0 ||
             count * 100 - answer.size() > 100) {
             count++;
             answer = answer + '\n';
