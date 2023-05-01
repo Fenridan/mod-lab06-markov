25115fc897a331a3b5892c507ce26ed2daafe747
Roman Denisov <99988283+Fenridan@users.noreply.github.com>
2023-05-02 01:29:14 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -6,13 +6,13 @@
 #include <vector>
 #include <map>
 class Gen {
-private:
+ private:
     std::string data;
     std::vector<std::string> words;
     typedef std::deque<std::string> prefix;
     int NPREF;
     int MAXGEN;
-public:
+ public:
     std::map<prefix, std::vector<std::string> > statetab;
     Gen(std::string arr, int val1, int val2);
     std::string getText();
