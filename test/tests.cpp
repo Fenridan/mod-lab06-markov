de3a2eb2d9b781f827429b37f9ee50aa28c54b15
Roman Denisov <99988283+Fenridan@users.noreply.github.com>
2023-05-01 21:48:57 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,14 @@
 // Copyright 2021 GHA Test Team
 #include <gtest/gtest.h>
+#include "textgen.h"
+
+TEST(prefix_count, first) {
+    string t = "first second third fourth fifth sixth seventh eighth ninth tenth";
+    int nPref = 2;
+    int maxGen = 10;
+    Gen m = Gen(t, nPref, maxGen);
+    auto table = m.statetab;
+    for (const auto& element : table) {
+        EXPECT_EQ(element.first.size(), nPref);
+    }
+}
